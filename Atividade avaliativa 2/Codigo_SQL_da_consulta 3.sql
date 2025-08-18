86c657ba5dc2c23ceaca6f9d1de6ae3307ccd2e7
Mateus Alencar Ferreira, MSc, MBA <86336670+ferreiramateusalencar@users.noreply.github.com>
2025-08-18 20:34:04 -0300

    Update Codigo_SQL_da_consulta 3.sql

--- Atividade avaliativa 2/Codigo_SQL_da_consulta 3.sql
+++ Atividade avaliativa 2/Codigo_SQL_da_consulta 3.sql
@@ -11,7 +11,16 @@ SELECT nomem FROM clinica.medico
 
 WHERE sexo = 'M' AND dt_nasc BETWEEN '1970-01-01' AND '1979-12-31'
 
-ORDER BY nomem
-
+ORDER BY nomem;
 
 -- Fim do Código SQL da consulta 3
+
+-- Código SQL da consulta 3 (Alternativo, com YEAR)
+
+SELECT nomem FROM clinica.medico
+
+WHERE sexo = 'M' AND (YEAR(dt_nasc) BETWEEN 1970 AND 1979)
+
+ORDER BY nomem;
+
+-- Fim do Código SQL da consulta 3 (Alternativo, com YEAR)
